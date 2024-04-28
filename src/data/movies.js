35b1d550550842b82e6ef70d3ff7a8f500b7998d
Chandana M 1036736. <Chandana.M@jda.com>
2024-04-28 18:30:05 +0530

    moviesApp

---
+++ src/data/movies.js
@@ -0,0 +1,92 @@
+export const movies = [
+    {
+        id: 1,
+        title: "The Matrix",
+        year: 1999,
+        director: "Lana Wachowski",
+        rating: "R",
+        runtime: 136,
+        poster:
+            "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+        description:
+            "Neo, a computer programmer and hacker, has always questioned the reality of the world around him. His suspicions are confirmed when Morpheus, a rebel leader, contacts him and reveals the truth to him.",
+    },
+    {
+        id: 2,
+        title: "The Lord of the Rings",
+        year: 2001,
+        director: "Peter Jackson",
+        rating: "PG-13",
+        runtime: 178,
+        poster:
+            "https://images-na.ssl-images-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
+        description:
+            "The Lord of the Rings is a trilogy of epic fantasy adventure films directed by Peter Jackson, based on the novel The Lord of the Rings by British author J. R. R. Tolkien. The films are subtitled The Fellowship of the Ring, The Two Towers, and The Return of the King.",
+
+    },
+    {
+        id: 3,
+        title: "The Dark Knight",
+        year: 2008,
+        director: "Christopher Nolan",
+        rating: "PG-13",
+        runtime: 152,
+        poster:
+            "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+        description:
+            "Batman has a new foe, the Joker, who is an accomplished criminal hell-bent on decimating Gotham City. Together with Gordon and Harvey Dent, Batman struggles to thwart the Joker before it is too late.",
+
+    },
+    {
+        id: 4,
+        title: "Inception",
+        year: 2010,
+        director: "Christopher Nolan",
+        rating: "PG-13",
+        runtime: 148,
+        poster:
+            "https://images-na.ssl-images-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+        description:
+            "Cobb steals information from his targets by entering their dreams. He is wanted for his alleged role in his wife's murder and his only chance at redemption is to perform a nearly impossible task.",
+
+    },
+    {
+        id: 5,
+        title: "Interstellar",
+        year: 2014,
+        director: "Christopher Nolan",
+        rating: "PG-13",
+        runtime: 169,
+        poster:
+            "https://images-na.ssl-images-amazon.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+        description:
+            "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.",
+
+    },
+    {
+        id: 6,
+        title: "The Dark Knight Rises",
+        year: 2012,
+        director: "Christopher Nolan",
+        rating: "PG-13",
+        runtime: 164,
+        poster:
+            "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_SX300.jpg",
+        description:
+            "Bane, an imposing terrorist, attacks Gotham City and disrupts its eight-year-long period of peace. This forces Bruce Wayne to come out of hiding and don the cape and cowl of Batman again.",
+
+    },
+    {
+        id: 7,
+        title: "The Lord of the Rings: The Fellowship of the Ring",
+        year: 2001,
+        director: "Peter Jackson",
+        rating: "PG-13",
+        runtime: 178,
+        poster:
+            "https://images-na.ssl-images-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
+        description:
+            "A ring with mysterious powers lands in the hands of a young hobbit, Frodo. Under the guidance of Gandalf, a wizard, he and his three friends set out on a journey and land in the Elvish kingdom.",
+
+    },
+];
